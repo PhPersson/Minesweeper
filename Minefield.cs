ea41984a850d96440105f6e20c51344bf328dc85
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-08-16 02:02:30 +0200

    Add project files.

---
+++ Minefield.cs
@@ -0,0 +1,17 @@
+﻿namespace Minesweeper;
+
+public class Minefield
+{
+
+    public int Rows { get; }
+    public int Columns { get; }
+    private bool[,] _bombLocations = new bool[5, 5];
+    private bool[,] _uncoveredCell = new bool[5, 5];
+
+    public Minefield(int rows, int cols)
+    {
+        Rows = rows;
+        Columns = cols;
+    }
+
+}
---
+++ Minesweeper.cs
@@ -0,0 +1,58 @@
+﻿namespace Minesweeper;
+
+public static class Minesweeper
+{
+    static void Main()
+    {
+        var field = new Minefield(5, 5);
+        bool gameOver = false;
+
+        //set the bombs...
+        //field.SetBomb(0, 0);
+        //field.SetBomb(0, 1);
+        //field.SetBomb(1, 1);
+        //field.SetBomb(1, 4);
+        //field.SetBomb(4, 2);s
+
+        //the mine field should look like this now:
+        //  01234
+        //4|1X1
+        //3|11111
+        //2|2211X
+        //1|XX111
+        //0|X31
+
+        // Game code...
+
+        Console.WriteLine("Minefield:");
+
+
+        do
+        {
+            var usersInput = GetUserInput();
+
+            if (usersInput?.Length == 2 && int.TryParse(usersInput[0], out int x) && int.TryParse(usersInput[1], out int y))
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+
+        }
+        while (!gameOver);
+    }
+
+    public static string[] GetUserInput()
+    {
+        Console.WriteLine("Your move: (X Y)");
+        string[] input = Console.ReadLine().Split(' ');
+        return input;
+    }
+
+
+
+}
+
+
